da840ecfb40cce54eca4abcc7aead6ffaaf9584c
Jorge <floreshfj@gmail.com>
2023-09-25 18:15:26 -0400

    segundo commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,13 @@
 
 namespace App\Exceptions;
 
-use Exception;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+// use Exception;
+// use Illuminate\Validation\ValidationException;
+// use Illuminate\Auth\Access\AuthorizationException;
+// use Illuminate\Database\Eloquent\ModelNotFoundException;
+// use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -23,6 +24,27 @@ class Handler extends ExceptionHandler
         ValidationException::class,
     ];
 
+        /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+        /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
     /**
      * Report or log an exception.
      *
